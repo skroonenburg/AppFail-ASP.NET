d03fec33f315ab3147fd7f25aabcfd045ef556c6
Sam Kroonenburg <skroonenburg@gmail.com>
2012-07-03 20:29:39 +1000

    Adding support for URL exact match filtering, and tilda notation URLs.

---
+++ src/AppFail/Helpers/UrlHelpers.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppfailReporting.Helpers
+{
+    internal static class UrlHelpers
+    {
+        public static string NormalizeUrl(this string url)
+        {
+            if (url == null)
+            {
+                return url;
+            }
+
+            if (url.StartsWith("~"))
+            {
+                return url.Substring(1, url.Length);
+            }
+
+            return url;
+        }
+
+        public static bool UrlsAreEqual(string urlA, string urlB)
+        {
+            if (urlA == null && urlB == null)
+            {
+                return true;
+            }
+
+            if ((urlA == null && urlB != null) || (urlB == null && urlA != null))
+            {
+                return false;
+            }
+
+            return urlA.NormalizeUrl().Equals(urlB.NormalizeUrl(), StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static bool UrlStartsWith(string url, string value)
+        {
+            if (url == null && value == null)
+            {
+                return true;
+            }
+
+            if ((url == null && value != null) || (value == null && url != null))
+            {
+                return false;
+            }
+
+            return url.ToUpperInvariant().StartsWith(value.NormalizeUrl().ToUpperInvariant());
+        }
+
+        public static bool UrlContains(string url, string value)
+        {
+            if (url == null && value == null)
+            {
+                return true;
+            }
+
+            if ((url == null && value != null) || (value == null && url != null))
+            {
+                return false;
+            }
+
+            return url.ToUpperInvariant().Contains(value.NormalizeUrl().ToUpperInvariant());
+        }
+
+    }
+}
